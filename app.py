34a48b2890a761413d5be8d9967ed1df6c80ab2f
dinbarss <dibar@LAPTOP-8AR32AF1.localdomain>
2025-12-01 09:36:04 +0200

    Fix some unnoticed code issues

--- app.py
+++ app.py
@@ -42,14 +42,14 @@ def create():
     password1 = request.form["password1"]
     password2 = request.form["password2"]
     if password1 != password2:
-        return render_template("login.html", error="Tunnus on jo varattu")
+        return render_template("login.html", error="Syöttämäsi salasanat eivät täsmää. Yritä uudelleen.")
     password_hash = generate_password_hash(password1)
 
     try:
         sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
         db.execute(sql, [username, password_hash])
     except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
+        return render_template("login.html", error="Tunnus on jo varattu")
 
     # Redirect to login after registration
     return redirect("/login")
