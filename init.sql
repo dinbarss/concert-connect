95cf8c0eac29cb13dcc0df52358ed7b035613375
dinbarss <dibar@LAPTOP-8AR32AF1.localdomain>
2025-12-01 05:02:09 +0200

    Add init.sql with sample music genres for ticket categorisation

---
+++ init.sql
@@ -0,0 +1,9 @@
+-- Clear existing categories
+DELETE FROM categories;
+
+-- Insert music genres
+INSERT INTO categories (name) VALUES ('K-pop');
+INSERT INTO categories (name) VALUES ('J-pop');
+INSERT INTO categories (name) VALUES ('K-pop Solo');
+INSERT INTO categories (name) VALUES ('K-Rock');
+INSERT INTO categories (name) VALUES ('K-hip-hop');
